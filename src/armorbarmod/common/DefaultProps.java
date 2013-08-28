3a7c695f49e0e694b414da10545cfeb51958dfb8
Tarion <ProjectZulu@outlook.com>
2013-08-28 11:19:24 -0300

    Revision++

--- src/armorbarmod/common/DefaultProps.java
+++ src/armorbarmod/common/DefaultProps.java
@@ -1,7 +1,7 @@
 package armorbarmod.common;
 
 public class DefaultProps {
-	public static final String VERSION_STRING = "0.8.2";
+	public static final String VERSION_STRING = "0.8.3";
 	public static final String modId = "mod_ArmorBarMod";
 	
     public static final String armorBarKey = "armorbar";
