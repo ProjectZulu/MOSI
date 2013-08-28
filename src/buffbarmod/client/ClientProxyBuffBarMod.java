df89d3806273acc4495283fa8514d867ea829273
Tarion <ProjectZulu@outlook.com>
2013-08-28 11:17:15 -0300

    Fix BuffBar Display creation before settings are loaded.

--- src/buffbarmod/client/ClientProxyBuffBarMod.java
+++ src/buffbarmod/client/ClientProxyBuffBarMod.java
@@ -10,13 +10,13 @@ public class ClientProxyBuffBarMod extends CommonProxyBuffBarMod{
 	@Override
 	public void registerRenderThings() {
 //		MinecraftForgeClient.preloadTexture("/mods/icons_BuffBarMod.png");
+        TickRegistry.registerTickHandler(new BuffBarDisplayTicker(), Side.CLIENT);
 	}
 		
 	//Pre-Init
 	@Override
 	public void registerTickers(){
 		//None Yet :(
-        TickRegistry.registerTickHandler(new BuffBarDisplayTicker(), Side.CLIENT);
 	}
 
 }
