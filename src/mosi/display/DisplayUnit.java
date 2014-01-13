408cfba2e755d965a97ce2ed0dc550f262af7a63
unknown <ProjectZulu@outlook.com>
2014-01-13 14:26:22 -0400

    Rearrrange method. No Functional change, simply changes how they are auto added to new isntances.

--- src/mosi/display/DisplayUnit.java
+++ src/mosi/display/DisplayUnit.java
@@ -1,5 +1,6 @@
 package mosi.display;
 
+import mosi.utilities.Coord;
 import net.minecraft.client.Minecraft;
 
 import org.lwjgl.util.Point;
@@ -12,15 +13,15 @@ public interface DisplayUnit {
      */
     public abstract String getType();
 
-    public abstract Point getPosition();
-
-    public abstract JsonObject saveCustomData(JsonObject jsonObject);
-
-    public abstract void loadCustomData(DisplayUnitFactory factory, JsonObject customData);
+    public abstract Coord getPosition();
 
     public void onUpdate(Minecraft mc, int ticks);
 
     public boolean shouldRender(Minecraft mc);
 
     public void renderDisplay(Minecraft mc, Point Position);
+
+    public abstract JsonObject saveCustomData(JsonObject jsonObject);
+
+    public abstract void loadCustomData(DisplayUnitFactory factory, JsonObject customData);
 }
